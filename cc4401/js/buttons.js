470df5524c6068d8ef56608ddb8bc568b67cb7a9
Matias Meneses <matias.imc@gmail.com>
2016-05-09 00:37:05 -0300

    fixed commit page

--- cc4401/js/buttons.js
+++ cc4401/js/buttons.js
@@ -5,14 +5,12 @@ function show_commits(project) {
     $('#page-wrapper').html('');
     $('#page-wrapper').append('<div class="row"> <div class="col-lg-12"> <h1 class="page-header">Commits - '+project+'</h1> </div></div>');
     for (var i in json) {
-      $('#page-wrapper').append('<div class="row"><div class="col-lg-12"> <div class="panel panel-primary"> <div class="panel-heading">'+json[i].nombre+' ('+json[i].user+')'+ '<div class="pull-right">Total: '+json[i].commits.length+'</div> </div><div class="panel-body"> ')
-      var body = "";
-      $('#page-wrapper').append('<p>what is love</p>');
+      var body = '<div class="row"><div class="col-lg-12"> <div class="panel panel-primary"> <div class="panel-heading">'+json[i].nombre+' ('+json[i].user+')'+ '<div class="pull-right">Total: '+json[i].commits.length+'</div> </div><div class="panel-body"> ';
       for (var j in json[i].commits) {
         body = body+'<p>message: '+json[i].commits[j].message+'<br>time: '+json[i].commits[j].time+'<br>url: <a target="_blank" href="'+json[i].commits[j].url+'">'+json[i].commits[j].url+'</a></p>';
       }
+      body = body+'</div></div></div></div>'
       $('#page-wrapper').append(body);
-      $('#page-wrapper').append('</div></div></div></div>');
     }
   })
 }
