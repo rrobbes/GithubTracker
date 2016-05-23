889ae837754c328d74edb8742f1b6de14599bae9
Michel Llorens <mllorens@dcc.uchile.cl>
2016-05-23 20:54:22 -0300

    Added branch, deletions and additions to commit display.

--- cc4401/js/buttons.js
+++ cc4401/js/buttons.js
@@ -7,7 +7,7 @@ function show_commits(project) {
   for (var i in json) {
     var body = '<div class="row"><div class="col-lg-12"> <div class="panel panel-primary"> <div class="panel-heading">'+json[i].nombre+' ('+json[i].user+')'+ '<div class="pull-right">Total: '+json[i].commits.length+'</div> </div><div class="panel-body"> ';
     for (var j in json[i].commits) {
-      body = body+'<p>message: '+json[i].commits[j].message+'<br>time: '+json[i].commits[j].time+'<br>url: <a target="_blank" href="'+json[i].commits[j].url+'">'+json[i].commits[j].url+'</a></p>';
+      body = body+'<p><b>message: '+json[i].commits[j].message+'</b><br>time: '+json[i].commits[j].time+'<br>branch: '+json[i].commits[j].branch+'<br>additions: <span class="text-success">'+json[i].commits[j].additions+'</span><br>deletions: <span class="text-danger">'+json[i].commits[j].deletions+'</span><br>url: <a target="_blank" href="'+json[i].commits[j].url+'">'+json[i].commits[j].url+'</a></p><hr>';
     }
     body = body+'</div></div></div></div>'
     $('#page-wrapper').append(body);
